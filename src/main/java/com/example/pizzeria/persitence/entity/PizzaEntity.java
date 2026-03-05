a9a0ed4cc3cc3bdf71042c1851f288447b2fd422
veron <veronicajugaaaa113@gmail.com>
2026-03-04 22:19:10 -0600

    dos

--- src/main/java/com/example/pizzeria/persitence/entity/PizzaEntity.java
+++ src/main/java/com/example/pizzeria/persitence/entity/PizzaEntity.java
@@ -1,9 +1,15 @@
 package com.example.pizzeria.persitence.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="pizza")
+@Getter
+@Setter
+@NoArgsConstructor
 public class PizzaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +18,20 @@ public class PizzaEntity {
 
     @Column(nullable = false, length = 30, unique = true)
     private String name;
+
+    @Column(nullable = false, length = 150)
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
+    private Double price;
+
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegetarian;
+
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegan;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean available;
+
 }
